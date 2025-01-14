fb03f87b7a8e6c76873b6eef9888a848cb030e86
shivanandgn033 <shivanandgn033@gmail.com>
2025-01-14 13:05:07 +0530

    font awesome svg icon usage in angular19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { library, icon } from '@fortawesome/fontawesome-svg-core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'fontawesome-demo';
+export class AppComponent implements OnInit {
+  coffeeIcon!: SafeHtml;
+  twitterIcon!: SafeHtml;
+  faCoffee = faCoffee;
+
+  constructor(private sanitizer: DomSanitizer) {
+    // Add icons to the library (only once, ideally in a service or app.module)
+    library.add(faCoffee, faTwitter);
+  }
+
+  ngOnInit(): void {
+    // Get the SVG icon as an HTML string
+    const coffeeSvg = icon(faCoffee).html[0];
+    const twitterSvg = icon(faTwitter).html[0];
+
+
+    // Sanitize the HTML to prevent security vulnerabilities
+    this.coffeeIcon = this.sanitizer.bypassSecurityTrustHtml(coffeeSvg);
+    this.twitterIcon = this.sanitizer.bypassSecurityTrustHtml(twitterSvg);
+  }
+
 }
